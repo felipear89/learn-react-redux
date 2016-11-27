0143a50390d92987554bc8d9dcbd5eef13c20a69
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2016-11-27 00:42:55 -0200

    add state on search_bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			term: ''
+		}
+	}
 	
 	render() {
-		// return <input onChange={this.onInputChange} />;
-		return <input onChange={event => console.log(event.target.value)} />;
-	}
-
-	onInputChange(event) {
-		console.log(event.target.value);
+		return (
+			<div>
+				<input onChange={event => this.setState({term: event.target.value}) } />
+				<br />
+				Value of the input: {this.state.term}
+			</div>
+		);
 	}
 }
 
