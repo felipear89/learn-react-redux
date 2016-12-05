0d55b08a3492f324a2b9016409dc8e812111c79e
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2016-12-05 08:22:39 -0200

    Add react-sparkLines dependency

--- weather/src/containers/weather_list.js
+++ weather/src/containers/weather_list.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine} from 'react-sparklines'
 
 class WeatherList extends Component {
 
+	renderWeather(cityData) {
+		const name = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp);
+
+
+
+		return (
+			<tr key={name} >
+				<td>{name}</td>
+				<td>{name}</td>
+				<td>{name}</td>
+			</tr>
+		);
+	}
+
 	render() {
 		return (
-			<table>
+			<table className="table table-hover" >
 				<thead>
 					<tr>
 						<th>City</th>
@@ -15,7 +31,7 @@ class WeatherList extends Component {
 					</tr>
 				</thead>
 				<tbody>
-
+					{ this.props.weather.map(this.renderWeather) }
 				</tbody>
 			</table>
 		);
