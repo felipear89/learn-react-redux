819a80ff3dac8051f8261f5b64bfb0aaddc9bc02
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2016-12-09 08:07:40 -0200

    Create html form and inputs

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -4,7 +4,27 @@ import { reduxForm } from 'redux-form';
 class PostsNew extends Component {
 	render() {
 		return (
-			<div>Create Form</div>
+			<form>
+				<h3>Create A New Post</h3>
+				
+				<div className="form-group" >
+					<label>Title</label>
+					<input type="text" className="form-control" />
+				</div>
+
+				<div className="form-group" >
+					<label>Categories</label>
+					<input type="text" className="form-control" />
+				</div>
+
+				<div className="form-group" >
+					<label>Content</label>
+					<textarea className="form-control" />
+				</div>
+
+				<button type="submit" className="btn btn-primary">Submit</button>
+
+			</form>
 		)
 	}
 }
