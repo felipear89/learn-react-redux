59df78326e6e8ac7e6bcc28c8977ce4ef4fe71ae
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2016-12-09 08:59:10 -0200

    bind inputForm with redux state

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
 	render() {
+		const { fields: { title, categories, content }, handleSubmit } = this.props;
+		// const title = this.props.fields.title
+		// const handleSubmit = this.props.handleSubmit
+
+		console.log(title);
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.props.createPost)} >
 				<h3>Create A New Post</h3>
 				
 				<div className="form-group" >
 					<label>Title</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...title} />
 				</div>
 
 				<div className="form-group" >
 					<label>Categories</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...categories} />
 				</div>
 
 				<div className="form-group" >
 					<label>Content</label>
-					<textarea className="form-control" />
+					<textarea className="form-control" {...content} />
 				</div>
 
 				<button type="submit" className="btn btn-primary">Submit</button>
@@ -29,7 +36,10 @@ class PostsNew extends Component {
 	}
 }
 
+
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
 export default reduxForm({
 	form: 'PostsNewForm',
 	fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
