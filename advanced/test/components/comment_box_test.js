006ab3fa3e9e0a7d0b1c4af1e183ab10a94d5f92
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2016-12-22 08:26:58 -0200

    Add onChange evento in textarea and test it

--- advanced/test/components/comment_box_test.js
+++ advanced/test/components/comment_box_test.js
@@ -10,6 +10,22 @@ describe('CommentBox', () => {
 
 	it('has the correct class', () => {
 		expect(component).to.have.class('comment-box');
-	})
+	});
 
+	it('has a text area', () => {
+		expect(component.find('textarea')).to.exist;
+	});
+
+	describe('entering some text', () => {
+
+		beforeEach(() => {
+			component.find('textarea').simulate('change', 'new comment');
+		});
+		
+		it('show text that text in the textarea', () => {
+			expect(component.find('textarea')).to.have.value('new comment');
+		});
+
+	});
+	
 });
