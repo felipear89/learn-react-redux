91f98c5ec67172c445fd6ab715e8ce5c0cda0326
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2016-12-23 08:43:43 -0200

    Add comment_list

--- advanced/test/components/app_test.js
+++ advanced/test/components/app_test.js
@@ -2,13 +2,18 @@ import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
 describe('App' , () => {
-  let component;
+	let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+	beforeEach(() => {
+		component = renderComponent(App);
+	});
 
-  it('renders something', () => {
-    expect(component.find('.comment-box')).to.exist;
-  });
+	it('renders something', () => {
+		expect(component.find('.comment-box')).to.exist;
+	});
+
+	it('shows a comment list', () => {
+		expect(component.find('.comment-list')).to.exist;
+	});
+
 });
--- advanced/test/components/comment_box_test.js
+++ advanced/test/components/comment_box_test.js
@@ -26,6 +26,11 @@ describe('CommentBox', () => {
 			expect(component.find('textarea')).to.have.value('new comment');
 		});
 
+		it('when submitted, clears the input', () => {
+			component.simulate('submit');
+			expect(component.find('textarea')).to.have.value('');
+		});		
+
 	});
 	
 });
